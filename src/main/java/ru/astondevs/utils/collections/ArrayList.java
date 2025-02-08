f10f45cf653d67ca0ba44f90cc7527f8715afc1c
Satell1teGml <akaijanni1988.com>
2025-02-08 17:59:04 +0300

    add ArrayList

--- src/main/java/ru/astondevs/utils/collections/ArrayList.java
+++ src/main/java/ru/astondevs/utils/collections/ArrayList.java
@@ -1,5 +1,64 @@
 package ru.astondevs.utils.collections;
 
 
-public final class ArrayList {
+import java.util.Arrays;
+
+public final class ArrayList<T> {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public ArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(T element) {
+        ensureCapacity();
+        elements[size++] = element;
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+    }
+
+    public boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elements, size));
+    }
 }
