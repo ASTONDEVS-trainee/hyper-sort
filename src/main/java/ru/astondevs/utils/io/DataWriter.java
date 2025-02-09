7e125171d58a0f44e086600bd02d4407aebbd06c
TandIT <pro.tatiana@bk.ru>
2025-02-09 11:38:05 +0300

    Add FileWriter

--- src/main/java/ru/astondevs/utils/io/DataWriter.java
+++ src/main/java/ru/astondevs/utils/io/DataWriter.java
@@ -1,5 +1,29 @@
 package ru.astondevs.utils.io;
+import java.util.*;
 
 
-public class DataWriter {
-}
+    public final class DataWriter {
+
+        private ArrayList array;
+        private Integer index;
+        private String path;
+        private Boolean append = true;
+
+
+        private DataWriter (String path, Boolean append){
+            this.path = path;
+            this.append = append;
+
+        }
+
+        public static ArrayList writeArray (ArrayList array){
+            writeArray(array);
+            return array;
+        }
+
+        public static Integer writeIndex (Integer index){
+            writeIndex(index);
+            return index;
+        }
+    }
+
