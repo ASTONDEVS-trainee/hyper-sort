556eb28b8bd31b2b9b37a3f5ba2df06c1a69741f
TandIT <pro.tatiana@bk.ru>
2025-02-09 21:36:51 +0300

    Add FileWriter

--- src/main/java/ru/astondevs/utils/io/DataWriter.java
+++ src/main/java/ru/astondevs/utils/io/DataWriter.java
@@ -1,20 +1,37 @@
 package ru.astondevs.utils.io;
 import java.util.*;
 
+public final class DataWriter {
 
-    public final class DataWriter {
+    private static List objectList;
+    private static String pathFile;
+    private static Boolean append = true;
 
-        private static byte[] textBytes;
-        private static Integer index;
-        private static String path;
-        private static Boolean append = true;
+    private DataWriter() {
+    }
 
-        private DataOutput (String path, Boolean append) {
-            this.path = path;
-            this.append = append;
-        }
+    private DataWriter(String pathFile, Boolean append) {
+        this.pathFile = pathFile;
+        this.append = append;
+    }
 
-        public static byte[] getTextBytes() {
-            return textBytes;
+    public static void writer(List objectList, String pathFile) {
+        for (Object i : objectList) {
+            i.toString();
+            //нужно ещё записать
         }
     }
+
+    @Override
+    public String toString() {
+        System.out.println("============================================" + '\'' +
+                        Object.class.getClass() + '\'' +
+                        "============================================" + '\'' +
+                        Object.class.getDeclaredFields() + ": "
+                //цикл для полей?
+                //+ вывести для них значение?
+        );
+    }
+}
+
+
