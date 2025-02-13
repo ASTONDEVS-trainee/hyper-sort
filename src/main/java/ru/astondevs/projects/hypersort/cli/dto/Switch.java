aefede99929645e84fd6d94ceddbdd7bb4c65c63
SciBourne <bourne-sci-hack@yandex.ru>
2025-02-13 14:56:35 +0300

    [ FEAT ] Add Switch

---
+++ src/main/java/ru/astondevs/projects/hypersort/cli/dto/Switch.java
@@ -0,0 +1,40 @@
+package ru.astondevs.projects.hypersort.cli.dto;
+
+
+public enum Switch {
+    // Default selector:
+    SILENT,
+    COMPLETE,
+    BACK,
+    BACK_TO_INPUT,
+    BACK_TO_CLASS,
+    EXIT,
+
+    // Selector CLASS:
+    ANIMAL,
+    BARREL,
+    HUMAN,
+
+    // Selector INPUT:
+    USER,
+    INPUT_USER,
+    FILE,
+    FILE_PATH,
+    FILE_COUNT_OBJECTS,
+    RANDOM,
+    RANDOM_COUNT_OBJECTS,
+
+    // Selector PROCESSING:
+    SAVE,
+    SAVE_PATH,
+    SORTED_SAVE,
+    SORTED_SAVE_PATH,
+    DISPLAY,
+    SORTED_DISPLAY,
+    DIFF_DISPLAY,
+    SORT,
+    SORT_BY_INT,
+    FIND,
+    INPUT_FIND,
+    CLEAR
+}
